d9ff3c9ec48460f99bf17af12d617cb1f157589e
Clare Buckley <bucklec2@aston.ac.uk>
2017-03-21 16:00:48 +0000

    Add register user to database. TODO: Add correct user type

--- register.php
+++ register.php
@@ -1,31 +1,41 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <link rel="stylesheet" href="css/index.css">
-    <title>FiLo System Register</title>
-  </head>
-  <body>
-    <header id = "main-header">
-      <h1>Welcome to the FiLo System</h1>
-      <h4><i>Report or find lost items</i></h4>
-    </header>
-    <header id = "secondary-header">
-      <h2>Register a new account:</h2>
-    </header>
-    <p><a href="index.php">Back</a></p>
-    <form>
-      <p> Title:
-      <select name="title">
-			<option value="Mr" selected="selected"> Mr </option>
-			<option value="Miss"> Miss </option>
-			<option value="Mrs"> Mrs </option>
-			<option value="Ms"> Ms </option>
-		</select></p>
-      <p> Forename: <input type="text" name="forname" size="20" maxlength="20" required /></p>
-      <p> Surname: <input type="text" name="surname" size="20" maxlength="20" required /></p>
-      <p> Username: <input type="text" name="username" size="20" maxlength="20" required /></p>
-      <p> Password: <input type="password" name="password" size="20" maxlength="20" required /></p>
-      <p><input type="submit" action="index.php" name="submit" value="Submit" /></p>
-    </form>
-  </body>
-</html>
+<?php
+//Get form data, all are required in register.html
+$title = $_POST['title'];
+if(isset($_POST['forename'])){
+  $forename = $_POST['forename'];
+}
+if(isset($POST['surname'])){
+  $surname = $_POST['surname'];
+}
+if(isset($_POST['username'])){
+  $username = $_POST['username'];
+}
+if(!empty(trim($_POST['password']))){
+  $password = $_POST['password']; #needs to be hashed
+}
+if(isset($_POST['email'])){
+  $email = $_POST['email'];
+}
+//Connect to database through PDO
+$db = new PDO("mysql:dbname=coursework; host=localhost","root","");
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+try{
+  //Insert record using form data
+$insert=$db->prepare("INSERT INTO user (`Username`, `Password`, `Title`, `Forename`, `Surname`, `Email`, `UserType`) VALUES(:pusername,
+  '".$password."','".$title."',:pforename,:pusername,:pemail,'Admin');" );
+  $insert->bindParam(':pusername',$username, PDO::PARAM_STR, 10);
+  $insert->bindParam(':pforename',$forename, PDO::PARAM_STR, 20);
+  $insert->bindParam(':psurname',$surname, PDO::PARAM_STR, 20);
+  $insert->bindParam(':pemail',$email, PDO::PARAM_STR, 30);
+  $insert->execute();
+  echo "Added record successfully";
+}
+catch(PDOException $exception) {
+  //Catch exception
+  ?>
+  <p>Sorry, an error has occured. Please try again, or contact IT services if problems persist.</p>
+  <p>Error details: <?= $exception->getMessage(); ?></p>
+  <?php
+}
+ ?>
